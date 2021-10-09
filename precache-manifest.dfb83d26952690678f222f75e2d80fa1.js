83a59b9332a2e904ed679ba85773b9341c514029
dale wesdorp <dalewesdorp@gmail.com>
2021-10-09 16:22:02 +0200

    Updates

---
+++ precache-manifest.dfb83d26952690678f222f75e2d80fa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e94ee57aa9d1b4d33980303b8e1cf9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c4c4e80f74c20244b3de",
+    "url": "./static/css/main.4447b23d.chunk.css"
+  },
+  {
+    "revision": "9ccece0287c98ffdb8e2",
+    "url": "./static/js/2.209c1f48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.209c1f48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4c4e80f74c20244b3de",
+    "url": "./static/js/main.1bc6439e.chunk.js"
+  },
+  {
+    "revision": "008d1a3dcc00ae8cc55a",
+    "url": "./static/js/runtime-main.8964a01b.js"
+  }
+]);
